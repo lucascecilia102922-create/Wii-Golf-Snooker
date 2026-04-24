c53ef93a8445f0f75390384f6ade99f450e87976
lucascecilia102922-create <lucas.cecilia.102922@a.fecaf.com.br>
2026-04-24 10:04:56 -0300

    Add files via upload

---
+++ Wii Golf/Scripts/hole.gd
@@ -0,0 +1,48 @@
+extends Area2D
+@onready var ball_sprite = $"../../Ball and Club/Ball/Sprite Ball"
+@onready var ball = $"../../Ball and Club/Ball"
+@export var number : int
+
+var Speed = 125
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_instance_valid(ball):
+		if ball.winning:
+			win_fall(delta)
+	pass
+
+
+func _on_area_entered(area) -> void:
+	if area.is_in_group("Ball") and not ball.winning:
+		print("Ganhou")
+		ball.winning = true
+		ball.linear_velocity = Vector2.ZERO
+		ball.angular_velocity = 0
+		
+		
+
+			
+func win_fall(delta: float):
+	var ballx = move_toward(ball_sprite.global_position.x, global_position.x, delta*Speed)
+	var bally = move_toward(ball_sprite.global_position.y, global_position.y, delta*Speed)
+	
+	ball_sprite.global_position = Vector2(ballx,bally)
+	
+	ball_sprite.scale -= (Vector2(0.7, 0.7) * delta)
+	
+	if ball_sprite.scale.x <= 0.1:
+		#var spawned_ball = pre_ball.instantiate()
+		ball.queue_free()
+		#ball = spawned_ball
+		#get_parent().add_child(ball)
+		#ball_sprite = ball.get_node("AnimatedSprite2D")
+		#ball.global_position = start_pos
+		
+		
+		
