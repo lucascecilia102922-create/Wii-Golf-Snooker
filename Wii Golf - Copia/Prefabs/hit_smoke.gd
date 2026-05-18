53512fdba434151a56b385a0ad8d539bbe57d991
lucascecilia102922-create <lucas.cecilia.102922@a.fecaf.com.br>
2026-05-18 12:01:43 -0300

    Delete Wii Golf - Copia directory

--- Wii Golf - Copia/Prefabs/hit_smoke.gd
+++
@@ -1,7 +0,0 @@
-extends AnimatedSprite2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	self.play("Hit")
-	await self.animation_finished
-	self.queue_free()
--- Wii Golf - Copia/Scripts/global.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-@export var speed_pos = 160
-@export var connected = false
-@export var connecting = true
-@export var wii_acc = 0
-var wiimotes
